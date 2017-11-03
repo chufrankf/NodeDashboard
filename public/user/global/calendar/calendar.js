348fc5f194d0f4ab1f7d7cbea5e445ba0c5b54b7
chufrankf <chufrankf@gmail.com>
2017-11-02 22:15:51 -0700

    updated calender css

--- public/user/global/calendar/calendar.js
+++ public/user/global/calendar/calendar.js
@@ -1,70 +1,16 @@
 $(document).ready(function(){
     // page is now ready, initialize the calendar...
     $('#calendar').fullCalendar({
+        defaultView: 'month',
+        aspectRatio: 1.35,
         header: {
-            left: 'prev,next today',
-            center: 'title',
-            right: 'month,agendaWeek,agendaDay,listWeek'
+            left: 'title',
+            center: 'month,agendaWeek,agendaDay,listWeek',
+            right: 'prev,next today'
         },
         navLinks: true,
         editable: true,
-        eventLimit: true,
-        events: [
-            {
-                title: 'All Day Event',
-                start: '2017-10-01',
-            },
-            {
-                title: 'Long Event',
-                start: '2017-10-07',
-                end: '2017-10-10'
-            },
-            {
-                id: 999,
-                title: 'Repeating Event',
-                start: '2017-10-09T16:00:00'
-            },
-            {
-                id: 999,
-                title: 'Repeating Event',
-                start: '2017-10-16T16:00:00'
-            },
-            {
-                title: 'Conference',
-                start: '2017-10-11',
-                end: '2017-10-13'
-            },
-            {
-                title: 'Meeting',
-                start: '2017-10-12T10:30:00',
-                end: '2017-10-12T12:30:00'
-            },
-            {
-                title: 'Lunch',
-                start: '2017-10-12T12:00:00'
-            },
-            {
-                title: 'Meeting',
-                start: '2017-10-12T14:30:00'
-            },
-            {
-                title: 'Happy Hour',
-                start: '2017-10-12T17:30:00'
-            },
-            {
-                title: 'Dinner',
-                start: '2017-10-12T20:00:00'
-            },
-            {
-                title: 'Birthday Party',
-                start: '2017-10-13T07:00:00'
-            },
-            {
-                title: 'Click for Google',
-                url: 'http://google.com/',
-                start: '2017-10-28'
-            }
-        ]
+        eventLimit: true
     });
 });
   
