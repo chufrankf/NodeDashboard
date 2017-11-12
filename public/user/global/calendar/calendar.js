6e4f3b9d842fe65e4f1747e7d07e445d02694011
chufrankf <chufrankf@gmail.com>
2017-11-12 15:58:40 -0800

    add multi calendar support

--- public/user/global/calendar/calendar.js
+++ public/user/global/calendar/calendar.js
@@ -1,20 +1,25 @@
 $(document).ready(function(){
     // page is now ready, initialize the calendar...
-    var googleCalendar;
+    var calendarAddress = [];
     if(sessionStorage.user_settings && sessionStorage.user_settings != 'undefined' && sessionStorage.access_token  && sessionStorage.access_token != 'undefined'){
-        googleCalendar = $.grep(JSON.parse(sessionStorage.user_settings), function(x){
+        calendarAddress = $.grep(JSON.parse(sessionStorage.user_settings), function(x){
             return x.setting == 'gcal_address';
         });
 
-        $.notify("Loaded " + googleCalendar[0].value, {position: 'bottom left', className: 'success'});
+        $.notify("Loading Google Calendars: " + calendarAddress[0].value, {position: 'bottom left', className: 'success'});
+
+        calendarAddress = calendarAddress[0].value.split(';').map(function(x, i){
+            var address = {};
+            address.googleCalendarId = x.trim();
+            address.className = 'bgc' + (i % 10);
+            return address;
+        });
         
         ajax_configs_get({key: 'google_api_key'}, function(res){
             if(res.success){ 
                 $('#calendar').fullCalendar({
                     googleCalendarApiKey: res.result,
-                    events: {
-                        googleCalendarId: googleCalendar[0].value
-                    },
+                    eventSources: calendarAddress,
                     defaultView: 'month',
                     aspectRatio: 1.35,
                     header: {
